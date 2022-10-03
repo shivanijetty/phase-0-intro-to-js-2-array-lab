30be0b39a263a40b785c8ba0f76ac8638a259597
Shivani Jetty <shivanijetty@gmail.com>
2022-10-03 12:13:09 -0400

    commit tree 1949afc38f2c55aa86d7cec2cdd82dc3c27240e1

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+function appendCat(name) {
+    const moreCats = [...cats];
+    moreCats.push("Broom");
+
+    return moreCats;
+}
+function prependCat(name) {
+    const moreCats = [...cats];
+    moreCats.unshift("Arnold");
+
+    return moreCats;
+}
+function removeLastCat() {
+    const newCats = [...cats]
+    newCats.pop();
+    return newCats;
+}
+function removeFirstCat() {
+    const newCats = [...cats]
+    newCats.shift();
+    return newCats;
+}
